b227b7b4b394eac8ddad4c3c2d762f536a146ba5
codeByHimanshu <115619706+codeByHimanshu@users.noreply.github.com>
2025-02-05 15:53:43 +0530

    Add sample bookings data and enhance Dashboard layout for better user experience

--- client/src/ChannelManager/pages/DashBoard.jsx
+++ client/src/ChannelManager/pages/DashBoard.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
 import Navbar from "../component/Navbar";
 
-export default function Dashboard() {
-    return (
-        <>
-            <Navbar />
-            <div className="bg-black">
-                <div className="text-white" >hmlooo hmooooo</div>
-            </div>
-        </>
+const Dashboard = () => {
+  const [bookings, setBookings] = useState([
+    { id: 1, name: "Aditi Sinha", guests: 2, nights: 1, room: "Standard", voucher: "1526812565/1", adr: 1688.7 },
+    { id: 2, name: "Vipin Trivedi", guests: 1, nights: 1, room: "Twin", voucher: "0140855249/1", adr: 1822.5 },
+    { id: 3, name: "Neshanth Pranav", guests: 2, nights: 1, room: "Deluxe", voucher: "NH25061383211084/2", adr: 3400.0 },
+    { id: 4, name: "Neshanth Pranav", guests: 3, nights: 1, room: "Deluxe", voucher: "NH25061383211084/1", adr: 4100.0 }
+  ]);
 
-    )
-}
+  return (
+    <>
+    <Navbar />
+  
+    </>
+  );
+};
+
+export default Dashboard;
