18bbf727eb516f531ff6e5f53d599d4ca0e51f50
codeByHimanshu <115619706+codeByHimanshu@users.noreply.github.com>
2025-03-25 13:17:46 +0530

    header modified

--- client/src/PropertyManager/Pcomponents/HeaderP.jsx
+++ client/src/PropertyManager/Pcomponents/HeaderP.jsx
@@ -26,7 +26,7 @@ export default function HeaderP() {
   };
 
   const navList = (
-    <ul className=" mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+    <ul className=" mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6  border-r-0">
       <Typography
         as="li"
         variant="small"
@@ -324,7 +324,7 @@ export default function HeaderP() {
 
   return (
     <>
-      <Navbar className=" bg-indigo-600 border-b border-indigo-700 text-white mx-auto max-w-full px-2 py-2 lg:px-6 lg:py-3">
+      <Navbar className=" bg-indigo-600 border-b rounded-none text-white mx-auto max-w-full px-2 py-2 lg:px-6 lg:py-3">
         <div className="container mx-auto flex flex-wrap items-center justify-between">
           <Typography
             as="a"
