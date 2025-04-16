9c935c4e6784e8939f4d51b5243239c73aec00e4
Ryaaan4321 <aryan056400@gmail.com>
2025-04-16 16:53:30 +0530

    chngessssss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,3 +18,45 @@ function App() {
 
 export default App;
 
+
+/*
+ hotelid: { type: String, ref: "UserLogin" },
+    roomname: {
+        type: [String],
+        required: true,
+        index: true
+    },
+    pricepernight: { type: Number, required: true },
+    roomnumber: { type: Number, required: true },
+    adults: {
+        type: Number,
+        required: true,
+        min: 1,
+    },
+    children: {
+        type: Number,
+        default: 0,
+        min: 0,
+    },
+
+    isavailable: {
+        type: Boolean,
+        default: true
+    },
+    images: {
+        type: [String],
+        default: []
+    },
+    roomnumber: {
+        type: Number,
+        unique: true,
+        required: true
+    },
+    amenties: {
+        type: [String],
+        required: true,
+    },
+    roomtype: {
+        enum: ["EP", "CP"]
+    }
+*/
