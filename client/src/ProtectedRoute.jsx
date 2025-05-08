b2b951be40af4f5a5965f46a2dab422baaddb1e8
Ryaaan4321 <aryan056400@gmail.com>
2025-05-08 12:59:15 +0530

    fix: update token retrieval in ProtectedRoute and add console logs for debugging

--- client/src/ProtectedRoute.jsx
+++ client/src/ProtectedRoute.jsx
@@ -1,7 +1,9 @@
 import { Navigate } from 'react-router-dom';
 
 function ProtectedRoute ({ children }) {
-  const token = localStorage.getItem("token"); 
+  console.log("protected route got called");
+  const token = localStorage.getItem("access_token"); 
+  console.log("token from the protected route = ",token);
   if (!token) {
     alert("you are not logged in!")
     return <Navigate to="/" replace />;
