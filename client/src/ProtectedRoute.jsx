f43e57fb37efef8a2e77962f28c3df2de5714b60
Ryaaan4321 <aryan056400@gmail.com>
2025-05-08 13:04:18 +0530

    ccccc

--- client/src/ProtectedRoute.jsx
+++ client/src/ProtectedRoute.jsx
@@ -3,7 +3,6 @@ import { Navigate } from 'react-router-dom';
 function ProtectedRoute ({ children }) {
   console.log("protected route got called");
   const token = localStorage.getItem("access_token"); 
-  console.log("token from the protected route = ",token);
   if (!token) {
     alert("you are not logged in!")
     return <Navigate to="/" replace />;
