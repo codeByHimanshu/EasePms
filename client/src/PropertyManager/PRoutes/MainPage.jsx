25c125bdd4456bde5238f0406467f051f90c7efc
Ryaaan4321 <aryan056400@gmail.com>
2025-05-09 11:44:56 +0530

    fix: remove Outlet component from MainPage and comment out localStorage.clear() in ProtectedRoute

--- client/src/PropertyManager/PRoutes/MainPage.jsx
+++ client/src/PropertyManager/PRoutes/MainPage.jsx
@@ -8,7 +8,6 @@ function MainPage() {
         <div>
         {/* <LoginPage/> */}
           <HeaderP />
-          <Outlet/>
         </div>
        
       </div>
--- client/src/ProtectedRoute.jsx
+++ client/src/ProtectedRoute.jsx
@@ -2,6 +2,7 @@ import { Navigate } from 'react-router-dom';
 
 function ProtectedRoute ({ children }) {
   console.log("protected route got called");
+  // localStorage.clear();
   const token = localStorage.getItem("access_token"); 
   if (!token) {
     alert("you are not logged in!")
