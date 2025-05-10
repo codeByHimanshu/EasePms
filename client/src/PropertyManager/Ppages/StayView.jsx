02bd9b7aa3bb8e5c5337246b58f69c4a7b6e6d7c
Ryaaan4321 <aryan056400@gmail.com>
2025-05-10 10:59:05 +0530

    feat: add BookingCalendar component and integrate it into StayView page; update package dependencies for fullcalendar and date-fns

--- client/src/PropertyManager/Ppages/StayView.jsx
+++ client/src/PropertyManager/Ppages/StayView.jsx
@@ -1,45 +1,46 @@
 import { useEffect } from "react";
+import BookingCalendar from "../Pcomponents/BoookingCalendar";
 
 const StayView = () => {
 
-    useEffect(() => {
-        const  token = localStorage.getItem("access_token")
-        const fetchData = async () => {
-          try {
-            const response = await fetch("http://localhost:3000/api/reservation/getallreservations", {
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json",
-                "Authorization":`Bearer ${token}`
-              },
-              
-            });
-      console.log(response + "response from stayview");
-      
-            if (!response.ok) {
-              throw new Error("Failed to fetch data");
-            }
-      
-            const data = await response.json();
-            console.log("Fetched data:", data);                
-          } catch (error) {
-            console.error("Error fetching data:", error);
-          }
-        };
-      
-        fetchData();
-      }, []);
+  useEffect(() => {
+    const token = localStorage.getItem("access_token")
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/reservation/getallreservations", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+          },
+
+        });
+        console.log(response + "response from stayview");
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        const data = await response.json();
+        console.log("Fetched data:", data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
 
 
-    return (
-        <div className="p-4 bg-white shadow-lg rounded-lg">
-         
+  return (
+    <div className="p-4 bg-white shadow-lg rounded-lg">
 
-         
-        <h1> i am Stay View Page </h1>
-        </div>
-    );
+      <BookingCalendar />
+
+      <h1> i am Stay View Page </h1>
+    </div>
+  );
 };
 
 
